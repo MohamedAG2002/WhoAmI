c95960fd1a2ff76361db3bf53337d9e971402200
MohamedAG2002 <moonimooni132@gmail.com>
2024-03-12 23:59:32 +0200

    Remove some not-needed dirs

--- build/CMakeFiles/WhoAmI.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for WhoAmI.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/WhoAmI.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/WhoAmI.dir/src/main.cpp.o: \
- /home/mohamed/Dev/C++/Raylib/WhoAmI/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/mohamed/Dev/C++/Raylib/WhoAmI/src/core/game.h
